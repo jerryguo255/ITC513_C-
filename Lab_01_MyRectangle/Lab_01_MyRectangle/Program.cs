e616a1c34490fd6e330d23b73921c3346c567114
Zijie Guo <32231346+jerryguo255@users.noreply.github.com>
2018-08-05 11:59:21 +1200

    Add files via upload

---
+++ Lab_01_MyRectangle/Lab_01_MyRectangle/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_01_MyRectangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            List<Rectangle> rlist = new List<Rectangle>();
+            while (input != "#")
+            {
+                string[] parts = input.Split(',');
+                double width = double.Parse(parts[0]);
+                double height = double.Parse(parts[1]);
+                Rectangle a = new Rectangle(width, height);
+                rlist.Add(a);
+                
+                input = Console.ReadLine();
+            }
+            foreach (Rectangle sa in rlist)
+            {
+                Console.WriteLine(sa);
+                Console.WriteLine("Perimeter: "+sa.CaclPerimeter());
+                Console.WriteLine("Area: "+sa.CaclAreas());
+                Console.WriteLine("#");
+            }
+            
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Lab_01_MyRectangle/Lab_01_MyRectangle/Rectangle.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_01_MyRectangle
+{
+    class Rectangle
+    {
+        private double width;
+        private double height;
+        public Rectangle(double width, double height)
+        {
+            this.width = width;
+            this.height = height;
+
+        }
+        public double Width
+        {
+            get
+            {
+                return width;
+            }
+            set
+            {
+                this.width = value;
+            }
+        }
+        public double Heidth
+        {
+            get
+            {
+                return height;
+            }
+            set
+            {
+                this.height = value;
+            }
+        }
+        public double CaclAreas()
+        {
+            return height * width;
+        }
+        public double CaclPerimeter()
+        {
+            return (height + width)*2;
+        }
+        public override string ToString()
+        {
+            return string.Format("{0,0} x {1,0} Rectangle", Width, Heidth);
+
+        }
+    }
+}
