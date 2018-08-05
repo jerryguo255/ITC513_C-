b5d947258a29bba1bf94dce436e74678e12cce7e
Zijie Guo <32231346+jerryguo255@users.noreply.github.com>
2018-08-05 12:01:36 +1200

    Add files via upload

---
+++ Lab_02_TransactionLines/Lab_02_ TransactionLines/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_02__TransactionLines
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             string n = "";
+             double c = 0;
+             int q = 0;
+            string input = Console.ReadLine();
+            while (input != "#")
+            {
+                string[] parts = input.Split(',');
+                n = parts[0];
+                c = double.Parse(parts[1]);
+                q = int.Parse(parts[2]);
+                Transaction t = new Transaction(n, c, q);
+                Console.WriteLine(t);
+
+                input = Console.ReadLine();
+            }
+        }
+    }
+}
