4c5bb4511adbd6cd175af8a8a7ae609a3acb72b7
Jerry Guo <jerryguo255@outlook.com>
2019-01-23 20:59:10 +1300

    add lab3 and 4

---
+++ Lab_03_AnimalSpecies_I/Lab_03_AnimalSpecies_I/Animal.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_03_AnimalSpecies_I
+{
+    class Animal
+    {
+        private string species;
+        private int legs;
+        private string classification;
+        private bool isExtinct;
+        public string Species
+        {
+            set { species = value; }
+            get { return species; }
+        }
+        public int Legs
+        {
+            set { legs = value; }
+            get { return legs; }
+        }
+        public string Classification
+        {
+            set { classification = value; }
+            get { return classification; }
+        }
+        public bool IsExtinct
+        {
+            set { isExtinct = value; }
+            get { return isExtinct; }
+        }
+        public Animal(string Species, int Legs, string Classification, bool IsExtinct)
+        {
+            species = Species;
+            legs = Legs;
+            classification = Classification;
+            isExtinct = IsExtinct;
+         }
+        
+        public override string ToString()
+        {
+            if (isExtinct)
+            {
+                return species + " (" + legs + " legged) " + classification +" (extinct)";
+            }//dodo (2 legged) bird (extinct)
+            else
+            {
+                return species+ " (" + legs + " legged) " + classification;
+            }
+        }
+        
+        
+    }
+}
---
+++ Lab_04_AnimalSpecies_II/Lab_04_/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Threading;
+
+namespace Lab_04_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Animal> animalss =  GetData();
+             string input = Console.ReadLine();
+             while (input != "#")
+             {
+                 foreach (Animal line in animalss)
+                 {
+                     if (input == line.Classification)
+                     {
+                         Console.WriteLine(line);
+                     }
+                 }
+                 input = Console.ReadLine();
+             }
+            //Console.ReadLine();
+        }
+        static List<Animal> GetData()
+        {
+            List <Animal> animals = new List<Animal>();
+            //string aFileText = File.ReadAllText(@"../../animals.txt");
+            string aFileText = File.ReadAllText("animals.txt");
+            string[] lines  =  aFileText.Split('\n');   // 每一行 的内容  存进数组  lines // lines[0]
+            foreach (string line in lines)      
+            {
+                string [] parts = line.Split(',');//antelope,4,mammal,false
+                Animal a = new Animal(parts[0], int.Parse(parts[1]), parts[2], bool.Parse(parts[3]));
+                //Console.WriteLine(a.Species);
+                animals.Add(a);
+            }
+            return animals;
+        }
+    }
+}
